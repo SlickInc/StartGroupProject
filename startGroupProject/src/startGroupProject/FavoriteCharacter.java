a8476d4d219a78a2fd218a299a12fe924ff849a0
Ivan <ivennsfroese@gmail.com>
2021-10-20 13:56:53 -0500

    Add Input

--- startGroupProject/src/startGroupProject/FavoriteCharacter.java
+++ startGroupProject/src/startGroupProject/FavoriteCharacter.java
@@ -1,9 +1,11 @@
 package startGroupProject;
 
+import java.util.Scanner;
+
 public class FavoriteCharacter {
 
 	public static void main(String[] args) {
-		
+		Scanner inputScanner = new Scanner(System.in);
 
 	}
 
