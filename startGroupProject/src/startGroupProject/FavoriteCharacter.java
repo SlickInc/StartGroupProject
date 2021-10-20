6835d481a2f99eaabdc42c543d0189bdc7db37c6
Ivan <ivennsfroese@gmail.com>
2021-10-20 14:01:33 -0500

    Change name of input

--- startGroupProject/src/startGroupProject/FavoriteCharacter.java
+++ startGroupProject/src/startGroupProject/FavoriteCharacter.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class FavoriteCharacter {
 
 	public static void main(String[] args) {
-		Scanner inputScanner = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 
 	}
 
